c432b31b25f5c7f42fe7c3e15dd854a844d6711d
John Slowik <john@jslowik.com>
2025-12-30 15:19:16 -0600

    Initial deploy placeholders

--- services/hardware-controller/src/main.rs
+++ services/hardware-controller/src/main.rs
@@ -1,3 +1,16 @@
+use rppal::gpio::Gpio;
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+const GPIO_LED: u8 = 23;
 fn main() {
-    println!("Hello, world!");
+    println!("Blinking LED on GPIO {}.", GPIO_LED);
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into + output();
+
+    loop {
+        pin.toggle();
+        thread::sleep(Duration::from_secs(2));
+    }
 }
